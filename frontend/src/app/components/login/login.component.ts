bdc400a89b652d67e283441e7d319b3868ddd9fb
Lluc Valério <lluc.valerio@gmail.com>
2021-09-10 00:50:36 +0200

    Add logout service call on header template and typescript

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -14,6 +14,7 @@ import {Message,MessageService} from 'primeng/api';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
   loading = false;
