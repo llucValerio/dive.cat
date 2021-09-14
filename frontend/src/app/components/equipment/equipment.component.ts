7bdf6c458f3015c1c9c244391e1d04bc467f23ba
Lluc Valério <lluc.valerio@gmail.com>
2021-09-14 21:17:03 +0200

    Fix bug on equipment

--- frontend/src/app/components/equipment/equipment.component.ts
+++ frontend/src/app/components/equipment/equipment.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Immersion, User } from 'src/app/models';
+import { Equipment, User } from 'src/app/models';
 
 import { Message } from 'primeng/api';;
 
@@ -10,9 +10,8 @@ import { Message } from 'primeng/api';;
   styleUrls: ['./equipment.component.scss']
 })
 export class EquipmentComponent implements OnInit {
-
   user!: User;
-  immersions!: [Immersion];
+  equipment!: [Equipment];
   // vars used to control data load on display component
   loading = false;
   // messages array
@@ -21,20 +20,10 @@ export class EquipmentComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void { 
-    // debugger
-    // order immersions by date
     this.user = JSON.parse(localStorage.getItem('userData') || '')[0];
-    this.immersions  = this.user.immersions.sort((a:any,b:any)=>{
-      if (a.date>b.date){
-        return -1
-      }
-      if (a.date<b.date){
-        return 1
-      }
-      return 0
-    })
-
+    this.equipment  = this.user.equipment;
   }
+
   setMessage(severity: string, summary: string, detail:string): void{
     this.msgs1 = [
       ...this.msgs1,
@@ -54,27 +43,5 @@ export class EquipmentComponent implements OnInit {
         this.setMessage(`error`,`Error`, `${error.status} - ${error.statusText}`)
       break;
     }
-  }
-
-  setBottomTimeCard(immersionIndex:number): number {
-    if (this.immersions[immersionIndex].endHour === this.immersions[immersionIndex].startHour) {
-      return (this.immersions[immersionIndex].endMinut - this.immersions[immersionIndex].startMinut);  
-     } else{
-       return (this.immersions[immersionIndex].endMinut+(60-this.immersions[immersionIndex].startMinut));  
-     }
-  }
-
-  setDepthCard(immersionIndex:number): number {
-    return this.immersions[immersionIndex].immersionStages.
-    reduce((acc, immersion) => acc = acc > immersion.deep ? acc : immersion.deep, 0);
-  }
-
-  setValidatorCard(immersionIndex:number): string {
-    for (let index:number=0; index<this.immersions[immersionIndex].buddies.length; index++){
-      if (this.immersions[immersionIndex].buddies[index].supervisor) {
-        return this.immersions[immersionIndex].buddies[index].buddie.name;
-      }
-    }
-    return 'not validated'
   }
 }
